658052e691a660637cbea2d42735637dd2b0e8e9
Andrej <andrej31.10.92@rambler.ru>
2021-12-30 09:18:42 +0300

    not authentication

--- cloud-storage-nov-client/src/main/java/com/geekbrains/MainController.java
+++ cloud-storage-nov-client/src/main/java/com/geekbrains/MainController.java
@@ -1,10 +1,12 @@
 package com.geekbrains;
 
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
@@ -13,6 +15,7 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -21,11 +24,22 @@ import java.util.stream.Collectors;
 @Slf4j
 public class MainController implements Initializable {
 
+
     private static final int BUFFER_SEZE = 1024;
     private byte [] buf;
+    private String BufCopy = null;
+     private String BufCopy_server = null;
+    private Path path_copy = null;
+    private String path_copy_server = null;
     private Path clientDir;
+    public TableView<File_Info> clientView2;
     public ListView<String> clientView;
+    public TableView<File_Info> serverView2;
     public ListView<String> serverView;
+    public TextField path_Field;
+    public TextField path_server_Field;
+
+
     public TextField input;
     private DataInputStream is;
     private DataOutputStream os;
@@ -38,21 +52,63 @@ public class MainController implements Initializable {
             if (!Files.exists(clientDir)) {
                 Files.createDirectory(clientDir);
             }
+        TableColumn<File_Info, String> fileTypeColumn = new TableColumn<>("Тип");
+        fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
+        fileTypeColumn.setPrefWidth(30);
 
-            clientView.getItems().clear();
-            clientView.getItems().addAll(getFiles(clientDir));
-            clientView.setOnMouseClicked(event -> {
-                if (event.getClickCount() == 2) {
-                    String item = clientView.getSelectionModel().getSelectedItem();
-                    input.setText(item);
+        TableColumn<File_Info, String> fileNameColumn = new TableColumn<>("Имя");
+        fileNameColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFilename()));
+        fileNameColumn.setPrefWidth(150);
+
+        TableColumn<File_Info, Long> fileSizeColumn = new TableColumn<>("Размер");
+        fileSizeColumn.setCellValueFactory(param -> new SimpleObjectProperty<>(param.getValue().getSize()));
+
+        fileSizeColumn.setCellFactory(column -> {
+            return new TableCell<File_Info, Long>() {
+                @Override
+                protected void updateItem(Long item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                        setStyle("");
+                    } else {
+                        String text = String.format("%,d bytes", item);
+                        if (item == -1L) {
+                            text = "[DIR]";
+                        }
+                        setText(text);
+                    }
                 }
-            });
+            };
+        });
+        fileSizeColumn.setPrefWidth(110);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        TableColumn<File_Info, String> fileDateColumn = new TableColumn<>("Дата изменения");
+        fileDateColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getLastModified().format(dtf)));
+        fileDateColumn.setPrefWidth(150);
+
+        clientView2.getColumns().addAll(fileTypeColumn, fileNameColumn, fileSizeColumn, fileDateColumn);
+        clientView2.getSortOrder().add(fileTypeColumn);
+
+            updateList(clientDir);
             Socket socket = new Socket("localhost", 8189);
             is = new DataInputStream(socket.getInputStream());
             os = new DataOutputStream(socket.getOutputStream());
             Thread readThread = new Thread(this::read);
             readThread.setDaemon(true);
             readThread.start();
+            serverView.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2) {
+                    String file = path_server_Field.getText().concat("/" + serverView.getSelectionModel().getSelectedItem());
+                    log.debug("Dable clicket to = {}", file);
+                    try {
+                        inDirectory(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -70,6 +126,15 @@ public class MainController implements Initializable {
             while (true) {
                 String command = is.readUTF();
                 log.debug("Received: {}", command);
+
+                if (command.equals("path_list")) {
+                    String path_list = is.readUTF();
+                    Platform.runLater(() -> {
+                        path_server_Field.setText(path_list);
+                    });
+                }
+
+
                 if (command.equals("list")) {
                     int count = is.readInt();
                     log.debug("Procces list files, count = {}", count);
@@ -77,8 +142,10 @@ public class MainController implements Initializable {
                     for (int i = 0; i < count; i++) {
                         String fileName = is.readUTF();
                         list.add(fileName);
+
                     }
                     Platform.runLater(() -> {
+
                         serverView.getItems().clear();
                         serverView.getItems().addAll(list);
                     });
@@ -88,26 +155,32 @@ public class MainController implements Initializable {
                     String fileName = is.readUTF();
                     log.debug("download file: {}", fileName);
                     long size = is.readLong();
+                    log.debug("Read {} bytes ", size);
                     long butchCount = (size + BUFFER_SEZE - 1)/BUFFER_SEZE;
-                    Path file = clientDir.resolve(fileName);
+                    Path file = Paths.get(path_Field.getText()).resolve(fileName);
                     try (OutputStream fos = new FileOutputStream(file.toFile())) {
-                        for (int i = 0; i < size; i++) {
+                        for (int i = 0; i < butchCount; i++) {
                             int read = is.read(buf);
                             fos.write(buf, 0, read);
 
                         }
                     }
-                    List<String> files = getFiles(clientDir);
-                    Platform.runLater(()-> {
-                        clientView.getItems().clear();
-                        clientView.getItems().addAll(files);
-                    });
+                    List<String> files = getFiles( Paths.get(path_Field.getText()));
+                    updateList( Paths.get(path_Field.getText()));
 
                 }
+
+
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void inDirectory(String file) throws IOException {
+        os.writeUTF("in_directory");
+        os.writeUTF("/" + file);
+        os.flush();
     }
 
     public void sendMessage(ActionEvent actionEvent) throws IOException {
@@ -119,12 +192,15 @@ public class MainController implements Initializable {
     }
 
     public void upload(ActionEvent actionEvent) throws IOException {
-        String fileName = clientView.getSelectionModel().getSelectedItem();
+        String fileName = clientView2.getSelectionModel().getSelectedItem().getFilename();
         os.writeUTF("file");
+        os.writeUTF(path_server_Field.getText());
         os.writeUTF(fileName);
-        long size = Files.size(clientDir.resolve(fileName));
+         long size = Files.size(Paths.get(path_Field.getText()).resolve(fileName));
+
+//        long size = Files.size(clientDir.resolve(fileName));
         os.writeLong(size);
-        Path file = clientDir.resolve(fileName);
+        Path file = Paths.get(path_Field.getText()).resolve(fileName);
         try (FileInputStream fis = new FileInputStream(file.toFile())) {
             while (fis.available() > 0) {
                 int read = fis.read(buf);
@@ -138,7 +214,189 @@ public class MainController implements Initializable {
         String fileName = serverView.getSelectionModel().getSelectedItem();
         os.writeUTF("fileRequest");
         os.writeUTF(fileName);
+        os.writeUTF(path_server_Field.getText());
         os.flush();
 
+    }
+public void updateList(Path path) {
+
+        try {
+            path_Field.setText(path.normalize().toAbsolutePath().toString());
+            clientView2.getItems().clear();
+            clientView2.getItems().addAll(Files.list(path).map(File_Info::new).collect(Collectors.toList()));
+            clientView2.sort();
+            clientView2.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2) {
+                    Path p = Paths.get(path_Field.getText()).resolve(clientView2.getSelectionModel().getSelectedItem().getFilename());
+                    if (Files.isDirectory(p)) {
+                        updateList(p);
+                    }
+                }
+            });
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Не удалось обновить список файлов", ButtonType.OK);
+            alert.showAndWait();
+        }
+    }
+//    public void updateList(Path path) {
+//
+//        try {
+//            path_Field.setText(path.normalize().toAbsolutePath().toString());
+//            clientView.getItems().clear();
+//            clientView.getItems().addAll(getFiles(path));
+////            clientView.setOnMouseClicked(event -> {
+////                if (event.getClickCount() == 2) {
+////                    String item = clientView.getSelectionModel().getSelectedItem();
+////                    input.setText(item);
+////                }
+////            });
+//            clientView.setOnMouseClicked(event -> {
+//                if (event.getClickCount() == 2) {
+//                    Path p = Paths.get(path_Field.getText()).resolve(clientView.getSelectionModel().getSelectedItem());
+//                    if (Files.isDirectory(p)) {
+//                        updateList(p);
+//                    }
+//                }
+//            });
+//        } catch (IOException e) {
+//            Alert alert = new Alert(Alert.AlertType.WARNING, "Не удалось обновить список файлов", ButtonType.OK);
+//            alert.showAndWait();
+//        }
+//    }
+
+    public void btnPathUpAction(ActionEvent actionEvent) {
+        Path upperPath = Paths.get(path_Field.getText()).getParent();
+        if (upperPath != null) {
+            updateList(upperPath);
+        }
+    }
+
+    public void btnExitAction(ActionEvent actionEvent) {
+        Platform.exit();
+    }
+
+    public void btnDelete(ActionEvent actionEvent) {
+
+        if (clientView2.isFocused() && clientView2.getSelectionModel().getSelectedItem().getFilename() != null) {
+            String fileName = clientView2.getSelectionModel().getSelectedItem().getFilename();
+            try {
+                Files.delete(Paths.get(path_Field.getText() + "/" + fileName));
+                updateList(Paths.get(path_Field.getText()));
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Не удалось удалить файл", ButtonType.OK);
+                alert.showAndWait();
+            }
+        } else if (serverView.isFocused() && serverView.getSelectionModel().getSelectedItem() != null) {
+            try {
+                String fileName = serverView.getSelectionModel().getSelectedItem();
+                os.writeUTF("delete_file");
+                os.writeUTF(path_server_Field.getText() + "/" + fileName);
+                os.flush();
+
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.WARNING, "Не удалось удалить файл", ButtonType.OK);
+                alert.showAndWait();
+            }
+        }
+        }
+
+    public void btnPathUpAction_server(ActionEvent actionEvent) throws IOException {
+        String path = path_server_Field.getText();
+        log.debug("UP path = {}", path);
+        os.writeUTF("btn_up");
+        os.writeUTF(path);
+        os.flush();
+    }
+
+    public void rename_file(ActionEvent actionEvent) {
+
+
+    }
+
+    public void copyFile(ActionEvent actionEvent) {
+        if (!clientView2.isFocused() && !serverView.isFocused()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Ни один файл не выбран", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        if (clientView2.isFocused()) {
+            if (clientView2.getSelectionModel().getSelectedItem().getFilename() != null) {
+                BufCopy = clientView2.getSelectionModel().getSelectedItem().getFilename();
+                path_copy = Paths.get(path_Field.getText()).resolve(BufCopy);
+                log.debug("Copy file = {}", BufCopy);
+
+            }
+        }
+
+        if (serverView.getSelectionModel().getSelectedItem() != null) {
+            BufCopy_server = serverView.getSelectionModel().getSelectedItem();
+            path_copy_server = path_server_Field.getText();
+            log.debug("Copy file = {}", BufCopy_server);
+            log.debug("clientView2 = {}", clientView2.isFocused());
+            log.debug("serverView = {}", serverView.getSelectionModel().getSelectedItem());
+        }
+    }
+
+    public void pasteFile(ActionEvent actionEvent) throws IOException {
+        if (clientView2.isFocused()) {
+            if (path_copy != null) {
+                Path p = Paths.get(path_Field.getText()).resolve(BufCopy);
+                Path p_copy = Paths.get(path_Field.getText()).resolve(BufCopy.concat("_copy"));
+                try {
+                    if (Files.exists(p)) {
+                        Files.copy(path_copy, p_copy);
+                        log.debug("Past file " + BufCopy + "copy");
+                    } else {
+                        Files.copy(path_copy, p);
+                        log.debug("Past file " + BufCopy);
+                    }
+                    updateList(Paths.get(path_Field.getText()));
+                } catch (IOException e) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно скопировать ", ButtonType.OK);
+                    alert.showAndWait();
+                }
+            }
+        }
+        if (serverView.isFocused()) {
+             String path_to_copy = path_server_Field.getText();
+            if (path_copy_server != null) {
+                os.writeUTF("copy_file");
+
+                os.writeUTF(path_copy_server);
+                os.writeUTF(BufCopy_server);
+                os.writeUTF(path_to_copy);
+                os.flush();
+            }
+        }
+
+    }
+
+    public void create_folder(ActionEvent actionEvent) {
+        try {
+            if (clientView2.isFocused()) {
+            if (Files.exists(Paths.get(path_Field.getText() + "/" + "Новая папка"))) {
+                int i = 1;
+                while (Files.exists(Paths.get(path_Field.getText() + "/" + "Новая папка" + i))) {
+                    i++;
+                }
+                Files.createDirectory(Paths.get(path_Field.getText() + "/" + "Новая папка" + i));
+                log.debug("Create directory  " + "Новая папка" + i);
+            } else {
+                Files.createDirectory(Paths.get(path_Field.getText() + "/" + "Новая папка"));
+            }
+
+                updateList(Paths.get(path_Field.getText()));
+            } else if (serverView.isFocused()) {
+                String path = path_server_Field.getText();
+                os.writeUTF("crt_folder");
+                log.debug("create folder in path = {}", path);
+                os.writeUTF(path);
+                os.flush();
+            }
+
+    } catch (IOException e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, "Неудалось создать папку ", ButtonType.OK);
+                alert.showAndWait();
+}
     }
 }
--- cloud-storage-nov-server/src/main/java/com/geekbrains/Handler.java
+++ cloud-storage-nov-server/src/main/java/com/geekbrains/Handler.java
@@ -1,5 +1,6 @@
 package com.geekbrains;
 
+import javafx.event.ActionEvent;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
@@ -34,8 +35,8 @@ public class Handler implements Runnable {
         log.debug("Set nick: {} for new client", name);
         isRunning = true;
         buf = new byte[BUFFER_SEZE];
-        serverDir = Paths.get("cloud-storage-nov-server", "server");
-        sendListFiles();
+        serverDir = Paths.get("cloud-storage-nov-server", "server").toAbsolutePath();
+        sendListFiles(serverDir);
     }
     private String getDate() {
         return formatter.format(LocalDateTime.now());
@@ -52,10 +53,40 @@ public class Handler implements Runnable {
                 String command = is.readUTF(); // wait data
                 log.debug("received: {}", command);
                 if (command.equals("file")) {
-                    processFileMessage();
+                    Path path = Paths.get(is.readUTF()).normalize();
+                    processFileMessage(path);
                 }
                 if (command.equals("fileRequest")) {
                     processFileRequest();
+                }
+                if (command.equals("btn_up")) {
+                    Path path = Paths.get(is.readUTF()).normalize();
+                    log.debug("Up path: {}", path);
+                    btnPathUpAction_server(path);
+                }
+                if (command.equals("in_directory")) {
+                    Path path = Paths.get(is.readUTF()).normalize();
+                    log.debug("Dable clicket to = {}", path);
+                    if (Files.isDirectory(path)) {
+                        sendListFiles(path);
+                    }
+                }
+                if (command.equals("crt_folder")) {
+                    Path path = Paths.get(is.readUTF()).normalize();
+                    create_folder_server(path);
+                }
+                if (command.equals("delete_file")) {
+                    String fileName = is.readUTF();
+                    try {
+                Files.delete(Paths.get(fileName).normalize());
+                sendListFiles(Paths.get(fileName).getParent());
+            } catch (IOException e) {
+                log.debug("Не удалось удалить файл = {}", fileName);
+                    }
+                }
+                if (command.equals("copy_file")) {
+                    Path path = Paths.get(is.readUTF()).normalize();
+                    copyPasteFile(path);
                 }
             }
         } catch (Exception e) {
@@ -63,13 +94,55 @@ public class Handler implements Runnable {
         }
     }
 
+    private void copyPasteFile(Path path) throws IOException {
+        String filename_copy = is.readUTF();
+        String path_to_copy = is.readUTF();
+        Path Path_to_copy = Paths.get(path_to_copy).normalize().resolve(filename_copy);
+        Path p_copy = Paths.get(path_to_copy).normalize().resolve(filename_copy + "_copy");
+        try {
+                    if (Files.exists(Path_to_copy)) {
+                        Files.copy(path, p_copy);
+                        log.debug("Past file = {}", p_copy);
+                    } else {
+                        Files.copy(path, Path_to_copy);
+                        log.debug("Past file = {}", path_to_copy);
+                    }
+                    sendListFiles(Paths.get(path_to_copy));
+                } catch (IOException e) {
+            log.debug("Не удалось скопировать файл = {}", filename_copy );
+        }
+
+
+
+    }
+
+    private void create_folder_server(Path path) {
+        try {
+        if (Files.exists(Paths.get(path + "/" + "Новая папка"))) {
+                int i = 1;
+                while (Files.exists(Paths.get(path + "/" + "Новая папка" + i))) {
+                    i++;
+                }
+                Files.createDirectory(Paths.get(path + "/" + "Новая папка" + i));
+                log.debug("Create directory  " + "Новая папка" + i);
+            } else {
+                Files.createDirectory(Paths.get(path + "/" + "Новая папка"));
+                log.debug("Create directory  " + "Новая папка");
+            }
+            sendListFiles(path);
+            } catch (IOException e) {
+            log.debug("Не удалось создать папку" );
+        }
+    }
+
     private void processFileRequest() throws IOException {
         String fileName = is.readUTF();
+        Path path_fild = Paths.get(is.readUTF()).normalize();
         os.writeUTF("file");
         os.writeUTF(fileName);
-        long size = Files.size(serverDir.resolve(fileName));
+        long size = Files.size(path_fild.resolve(fileName));
         os.writeLong(size);
-        Path file = serverDir.resolve(fileName);
+        Path file = path_fild.resolve(fileName);
         try (FileInputStream fis = new FileInputStream(file.toFile())) {
             while (fis.available() > 0) {
                 int read = fis.read(buf);
@@ -79,22 +152,31 @@ public class Handler implements Runnable {
         os.flush();
     }
 
-    private void processFileMessage() throws IOException {
+    private void processFileMessage(Path path) throws IOException {
         String fileName = is.readUTF();
         log.debug("download file: {}", fileName);
         long size = is.readLong();
+        log.debug("Read {} bytes", size);
         long butchCount = (size + BUFFER_SEZE - 1)/BUFFER_SEZE;
-        Path file = serverDir.resolve(fileName);
+        Path file = path.resolve(fileName);
         try (OutputStream fos = new FileOutputStream(file.toFile())) {
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < butchCount; i++) {
                 int read = is.read(buf);
                 fos.write(buf, 0, read);
 
             }
         }
+        sendListFiles(path);
     }
-    private void sendListFiles() throws IOException {
-        List<String> files = Files.list(serverDir).map(path -> path.getFileName().toString()).collect(Collectors.toList());
+    private void sendListFiles(Path path) throws IOException {
+
+        List<String> files = Files.list(path).map(p -> p.getFileName().toString()).collect(Collectors.toList());
+
+        String path_list = path.normalize().toAbsolutePath().toString();
+        os.writeUTF("path_list");
+        os.writeUTF(path_list);
+        os.flush();
+
         os.writeUTF("list");
         os.writeInt(files.size());
         for (String file : files) {
@@ -102,4 +184,18 @@ public class Handler implements Runnable {
         }
         os.flush();
     }
+
+    private void btnPathUpAction_server(Path path) throws IOException {
+
+        Path upperPath = path.getParent();
+        log.debug("upperPath = {}", upperPath);
+        if (upperPath != null) {
+            if (path.equals(serverDir)) {
+                sendListFiles(serverDir);
+            } else {
+                sendListFiles(upperPath);
+            }
+        }
+    }
+
 }
